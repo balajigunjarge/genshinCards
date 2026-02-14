1cf86debfaa59c6bed84535f375c441ef961137f
Balaji <balajirameshgunjarge@gmail.com>
2026-02-14 20:58:08 +0530

    feat: add Card component with associated styling for display and interaction.

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,18 +3,32 @@ const Card = (props) => {
     <div className="card">
       <img src={props.backgroundImage} alt="" className="background-img" />
       <section className="backdrop">
-      <div className="top-div">
-        
-      <img src={props.image} alt="" className="icon-img"/><h1>{props.id}{props.name}<br/><section className="region">{props.region}</section></h1>
-     </div>
-      <p>{props.about}</p>
+        <div>
+          <div className="top-div">
+            <img src={props.image} alt="" className="icon-img" />
+            <h1>
+              {props.id}
+              {props.name}
+              <br />
+              <section className="region">{props.region}</section>
+            </h1>
+          </div>
+          <div>
+            <p>{props.about}</p>
+          </div>
+          <div>
+             <a href={props.link} target="_blank">
+          <button>Learn More</button>
+        </a>
+          </div>
+        </div>
 
-      <a href={props.link} target="_blank">
-        <button>Learn More</button>
-      </a>
+        
+
+       
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Card 
+export default Card;
